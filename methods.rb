01e105e482cfe6424b21022e6e100967ba3d1d02
nilesh1168 <n.suryawanshi1168@gmail.com>
2023-05-31 22:03:57 -0400

    method exercises

---
+++ methods.rb
@@ -0,0 +1,21 @@
+def greeting(name)
+    puts "Hello #{name}"
+end
+
+def multiply(number_1, number_2)
+    number_1 * number_2
+end
+
+def scream(words)
+    words = words + "!!!!"
+    puts words
+end    
+# Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+greeting("Nilesh")   
+
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+multiply(1,2)
+
+
+# What will the following code print to the screen?
+scream("Yippeee")
